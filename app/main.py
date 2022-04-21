cbd9d2863c4ea0014f5852affe4ac43a2175e60f
neoconstel <neoconstel@gmail.com>
2022-04-21 22:18:13 +0100

    changed static path to templates

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template      
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="templates")
 
 @app.route("/")
 def home():
@@ -23,5 +23,5 @@ def rtl():
 def white():
     return render_template("white/index_1.html")
     
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
